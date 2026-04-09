a98d030c90a12cb4f7a4dec5a9ca40e5cdafcf26
Oluwagbemileke-1 <gbemioduselu@gmail.com>
2026-04-10 00:55:54 +0100

    This better be it

--- QR-main(django)/attendance/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('check-in/', views.check_in, name='check-in'),
-    path('my-attendance/', views.my_attendance, name='my-attendance'),
-    path('admin-dashboard/', views.admin_dashboard, name='admin-dashboard'),
-    path('<uuid:event_id>/live-count/', views.live_attendance, name='live-attendance'),
-]
